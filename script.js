9121ed9d6d1be1551c5a8b189199bf1f4918fc22
vlad <nekita.avdoshin@gmail.com>
2022-09-08 19:40:03 +0300

    add null

--- script.js
+++ script.js
@@ -19,12 +19,13 @@ function clock() {
     arrowMinutes.style.transform = `rotateZ(${minutes}deg)`
     arrowSeconds.style.transform = `rotateZ(${seconds}deg)`
 
-    smallHours.innerHTML = date.getHours();
-    smallMinutes.innerHTML = date.getMinutes();
-    smallSeconds.innerHTML = date.getSeconds();
 
-    document.querySelector('.day').innerHTML = date.getDate()
-    document.querySelector('.month').innerHTML = date.getMonth()
+    date.getHours() < 10 ? smallHours.innerHTML = "0" + date.getHours() : smallHours.innerHTML = date.getHours();
+    date.getMinutes() < 10 ? smallMinutes.innerHTML = "0" + date.getMinutes() : smallMinutes.innerHTML = date.getMinutes();
+    date.getSeconds() < 10 ? smallSeconds.innerHTML = "0" + date.getSeconds() : smallSeconds.innerHTML = date.getSeconds()
+
+    date.getDate() < 10 ?  document.querySelector('.day').innerHTML = "0" + date.getDate() : document.querySelector('.day').innerHTML = date.getDate();
+    date.getMonth() < 10 ? document.querySelector('.month').innerHTML = "0" + date.getMonth() : document.querySelector('.month').innerHTML = date.getMonth();
     document.querySelector('.year').innerHTML = date.getFullYear()
   },0)
 }
